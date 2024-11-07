402a087d1e7c8866155bd29b9dd8736728f5d285
Owen-Reynolds <oreynold@ashland.edu>
2024-11-06 20:50:10 -0500

    Started Email Check List

---
+++ homepage/emailList.js
@@ -0,0 +1,27 @@
+function emailList(){
+    emailInput = document.getElementById("emailFooterInput").value;
+    checkEmail(emailInput);
+}
+
+//If input has @ then shows valid html if not then 
+function checkEmail(emailInput){
+    if(emailInput.includes("@"))
+        emailJoin('Valid');
+    else
+        emailJoin('Invalid');
+}
+
+//Checks if 
+function emailJoin(check){
+    if(check == 'Valid'){
+        document.getElementById("validEmailBox").style.display = "flex";
+    }
+    else{
+        document.getElementById("invalidEmailBox").style.display = "flex";
+    }
+}
+
+function closeEmail(){
+    document.getElementById("").style.display = "none";
+}
+
