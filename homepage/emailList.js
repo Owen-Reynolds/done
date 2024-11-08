9907e75a12e862511ca81f563c8f86cf8b5095ae
OwenxOwen <oreynold@ashland.edu>
2024-11-08 11:42:00 -0500

    About Footer

--- homepage/emailList.js
+++ homepage/emailList.js
@@ -6,26 +6,14 @@ function emailList(){
 //If input has @ then shows valid html if not then 
 function checkEmail(emailInput){
     if(emailInput.includes("@"))
-        emailJoin('Valid');
-    else
-        emailJoin('Invalid');
-}
-
-//Checks if 
-function emailJoin(check){
-    if(check == 'Valid'){
         document.getElementById("validEmailBox").style.display = "flex";
-    }
-    else{
+        
+    else
         document.getElementById("invalidEmailBox").style.display = "flex";
-    }
-}
-
-function closeEmail(){
-    document.getElementById("").style.display = "none";
 }
 
 function closePopUp(){
     document.getElementById("validEmailBox").style.display = "none";
     document.getElementById("invalidEmailBox").style.display = "none";
 }
+
