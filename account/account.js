3fbba6ab8cbd00fd5a45bdfaf5032101f6f39015
OwenxOwen <oreynold@ashland.edu>
2024-12-02 15:28:40 -0500

    Simple Validation Account

---
+++ account/account.js
@@ -0,0 +1,7 @@
+function login(){
+    let email = document.getElementById('emailInput').value;
+    let password = document.getElementById('passwordInput').value;
+
+    alert(email);
+    alert(password);
+}
---
+++ account/createAccount.js
@@ -0,0 +1,7 @@
+function createAccount(){
+    let email = document.getElementById('emailInput').value;
+    let password = document.getElementById('passwordInput').value;
+
+    alert(email);
+    alert(password);
+}
