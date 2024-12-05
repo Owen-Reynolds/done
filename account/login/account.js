d199a81f636e737dc268763a4487a29965443fb9
OwenxOwen <oreynold@ashland.edu>
2024-12-04 22:16:01 -0500

    Better Email Verification

--- account/login/account.js
+++ account/login/account.js
@@ -2,6 +2,12 @@ function login(){
     let email = document.getElementById('emailInput').value;
     let password = document.getElementById('passwordInput').value;
 
-    alert(email);
-    alert(password);
+    let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if(emailPattern.test(email)){
+        alert(email)
+    }
+    else{
+        alert("This is not a valid email");
+    }
 }
