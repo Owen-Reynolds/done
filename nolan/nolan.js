007c71b00f9f2c61f4c083f4cd99d19b55690a00
OwenxOwen <oreynold@ashland.edu>
2024-12-12 08:39:15 -0500

    Added Footer to all Relevant Pages

--- nolan/nolan.js
+++ nolan/nolan.js
@@ -9,3 +9,25 @@ function selectImage(frontImageUrl, backImageUrl, itemText, itemPrice){
     localStorage.setItem('productHolder', JSON.stringify(productHolder));
 }
 
+function emailList(){
+    emailInput = document.getElementById("emailFooterInput").value;
+    checkEmail(emailInput);
+}
+
+//If input has @ then shows valid html if not then 
+function checkEmail(emailInput){
+
+    let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if(emailPattern.test(emailInput))
+        document.getElementById("validEmailBox").style.display = "flex";
+        
+    else
+        document.getElementById("invalidEmailBox").style.display = "flex";
+}
+
+function closePopUp(){
+    document.getElementById("validEmailBox").style.display = "none";
+    document.getElementById("invalidEmailBox").style.display = "none";
+
+}
--- owen/owen.js
+++ owen/owen.js
@@ -7,6 +7,28 @@ function selectImage(frontImageUrl, backImageUrl, itemText, itemPrice){
     productHolder.push({frontImage: frontImageUrl, backImage: backImageUrl, text: itemText, price: itemPrice});
     // Set it inside of the imageHolder Key
     localStorage.setItem('productHolder', JSON.stringify(productHolder));
-    
 }
+
+function emailList(){
+    emailInput = document.getElementById("emailFooterInput").value;
+    checkEmail(emailInput);
+}
+
+//If input has @ then shows valid html if not then 
+function checkEmail(emailInput){
+
+    let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if(emailPattern.test(emailInput))
+        document.getElementById("validEmailBox").style.display = "flex";
+        
+    else
+        document.getElementById("invalidEmailBox").style.display = "flex";
+}
+
+function closePopUp(){
+    document.getElementById("validEmailBox").style.display = "none";
+    document.getElementById("invalidEmailBox").style.display = "none";
+}
+
 
